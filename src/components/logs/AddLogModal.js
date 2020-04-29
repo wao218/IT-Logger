bd99bc294f9dc10c1da4d261cba62b08bf37ee7e
Wesley Osborne <wao218@lehigh.edu>
2020-04-29 10:07:40 -0400

    Added TechSelectOptions component

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import TechSelectOptions from '../techs/TechSelectOptions';
 
 const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
@@ -45,9 +46,7 @@ const AddLogModal = ({ addLog }) => {
           <div className="input-field">
             <select name="tech" value={tech} className="browser-default" onChange={(e) => setTech(e.target.value)}>
               <option value="" disabled>Select Technician</option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sam Smith">Sam Smith</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
